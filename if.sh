b5f7115b57299700d8598877b380928d816cb72b
ishgary <ishanttgarg@gmail.com>
2022-02-01 18:02:00 +0530

    if file updated

---
+++ if.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+vari="c"
+
+if [[ $vari == 'a' ]]
+then 
+echo your conditon is true
+elif [[ $vari = "c" ]]
+then
+    echo your condition is Elif
+else 
+echo your condition is false
+fi  
